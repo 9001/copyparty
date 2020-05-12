dab3407bebf8ef84cf2e52b978a70cd22eea7e5e
ed <s@ocv.me>
2020-05-13 00:44:23 +0200

    v0.4.0

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 3, 1)
-CODENAME = "docuparty"
-BUILD_DT = (2020, 5, 7)
+VERSION = (0, 4, 0)
+CODENAME = "NIH"
+BUILD_DT = (2020, 5, 13)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
