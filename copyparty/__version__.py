9f578bfec6fbff3eb1a108d43d318d94329af194
ed <s@ocv.me>
2021-02-12 07:06:38 +0000

    v0.7.5

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 7, 4)
+VERSION = (0, 7, 5)
 CODENAME = "keeping track"
-BUILD_DT = (2021, 2, 4)
+BUILD_DT = (2021, 2, 12)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
