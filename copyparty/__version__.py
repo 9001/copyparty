43a23bf7331566dc9458a01508321fd3e4ae6775
ed <s@ocv.me>
2021-03-03 01:28:32 +0100

    v0.9.1

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 9, 0)
+VERSION = (0, 9, 1)
 CODENAME = "the strongest music server"
-BUILD_DT = (2021, 3, 2)
+BUILD_DT = (2021, 3, 3)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
