aff8185f2e6b6de87a915429ac9b3d1f4f6e2895
ed <s@ocv.me>
2021-04-17 01:29:27 +0200

    v0.10.9

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 10, 8)
+VERSION = (0, 10, 9)
 CODENAME = "zip it"
-BUILD_DT = (2021, 4, 11)
+BUILD_DT = (2021, 4, 17)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
--- copyparty/web/up2k.js
+++ copyparty/web/up2k.js
@@ -737,7 +737,8 @@ function up2k_init(have_crypto) {
     function hashing_permitted() {
         if (multitask) {
             var ahead = st.bytes.hashed - st.bytes.uploaded;
-            return ahead < 1024 * 1024 * 128;
+            return ahead < 1024 * 1024 * 128 &&
+                st.todo.handshake.length + st.busy.handshake.length < 16;
         }
         return handshakes_permitted() && 0 ==
             st.todo.handshake.length +
