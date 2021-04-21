7e8daf650e3ee1da5e42cd5d86fc95f71c7282e7
ed <s@ocv.me>
2021-04-21 22:04:21 +0200

    v0.10.14

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 10, 13)
+VERSION = (0, 10, 14)
 CODENAME = "zip it"
-BUILD_DT = (2021, 4, 20)
+BUILD_DT = (2021, 4, 21)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
