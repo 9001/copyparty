0061d29534352e9dc783dd1734a660452332f7ba
ed <s@ocv.me>
2021-06-06 19:09:55 +0200

    v0.11.8

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 11, 7)
+VERSION = (0, 11, 8)
 CODENAME = "the grid"
-BUILD_DT = (2021, 6, 5)
+BUILD_DT = (2021, 6, 6)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
