941aedb177e77aa33da198abb9fd988fad9e486b
ed <s@ocv.me>
2021-09-01 23:48:01 +0200

    v0.13.12

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (0, 13, 11)
+VERSION = (0, 13, 12)
 CODENAME = "future-proof"
-BUILD_DT = (2021, 8, 30)
+BUILD_DT = (2021, 9, 1)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
