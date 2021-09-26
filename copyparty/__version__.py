cb81fe962cda4de470f572282d609c1178537fc9
ed <s@ocv.me>
2021-09-26 20:15:21 +0200

    v1.0.7

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-VERSION = (1, 0, 6)
+VERSION = (1, 0, 7)
 CODENAME = "sufficient"
 BUILD_DT = (2021, 9, 26)
 
--- copyparty/web/browser.js
+++ copyparty/web/browser.js
@@ -4364,7 +4364,7 @@ var unpost = (function () {
 
 		for (var a = n; a < n2; a++)
 			if (QS('#op_unpost a.n' + a))
-				req.push(r.files[a].vp);
+				req.push(uricom_dec(r.files[a].vp)[0]);
 
 		var links = QSA('#op_unpost a.n' + n);
 		for (var a = 0, aa = links.length; a < aa; a++) {
