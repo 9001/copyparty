fc970d2deac84a1aae0e683fff36fafcd9270a23
ed <s@ocv.me>
2021-09-26 19:36:19 +0200

    v1.0.6

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (1, 0, 5)
+VERSION = (1, 0, 6)
 CODENAME = "sufficient"
-BUILD_DT = (2021, 9, 19)
+BUILD_DT = (2021, 9, 26)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
