9001/copyparty

commit b265e598345267c861dfd7fc29a67dd13c26d2c0

spa filetab

copyparty/web/browser.js
@@ -157,6 +157,7 @@ ebi('op_cfg').innerHTML = (
 	'		<a id="thumbs" class="tgl btn" href="#" tt="in icon view, toggle icons or thumbnails$NHotkey: T">🖼️ thumbs</a>\n' +
 	'		<a id="dotfiles" class="tgl btn" href="#" tt="show hidden files (if server permits)">dotfiles</a>\n' +
 	'		<a id="ireadme" class="tgl btn" href="#" tt="show README.md in folder listings">📜 readme</a>\n' +
+	'		<a id="spafiles" class="tgl btn" href="#" tt="speedboost when not using the navpane;$Nturn it off if things arent loading somehow">spa</a>\n' +
 	'	</div>\n' +
 	'</div>\n' +
 	(have_zip ? (
@@ -1494,7 +1495,8 @@ function play(tid, is_ev, seek, call_depth) {
 		}
 
 		mpui.progress_updater();
-		pbar.drawbuf();
+		pbar.onresize();
+		vbar.onresize();
 		mpl.announce();
 		return true;
 	}
@@ -3433,6 +3435,7 @@ var treectl = (function () {
 		mentered = null,
 		treesz = clamp(icfg_get('treesz', 16), 10, 50);
 
+	bcfg_bind(r, 'spa', 'spafiles', true);
 	bcfg_bind(r, 'ireadme', 'ireadme', true);
 	bcfg_bind(r, 'dyn', 'dyntree', true, onresize);
 	bcfg_bind(r, 'dots', 'dotfiles', false, function (v) {
@@ -3636,7 +3639,7 @@ var treectl = (function () {
 
 	r.goto = function (url, push) {
 		get_tree("", url, true);
-		reqls(url, push, true);
+		r.reqls(url, push, true);
 	};
 
 	function get_tree(top, dst, rst) {
@@ -3805,12 +3808,12 @@ var treectl = (function () {
 			treegrow.call(this.previousSibling, e);
 			return;
 		}
-		reqls(this.getAttribute('href'), true);
+		r.reqls(this.getAttribute('href'), true);
 		r.dir_cb = tree_scrollto;
 		thegrid.setvis(true);
 	}
 
-	function reqls(url, hpush, no_tree) {
+	r.reqls = function (url, hpush, no_tree) {
 		var xhr = new XMLHttpRequest();
 		xhr.top = url;
 		xhr.hpush = hpush;
@@ -3954,7 +3957,7 @@ var treectl = (function () {
 			xhr.open('GET', '/?am_js', true);
 			xhr.send();
 
-			return reqls(get_evpath(), false, true);
+			return r.reqls(get_evpath(), false, true);
 		}
 
 		r.gentab(get_evpath(), ls0);
@@ -4985,15 +4988,29 @@ function wintitle(txt) {
 }
 
 
+ebi('path').onclick = function (e) {
+	var a = e.target.closest('a[href]');
+	if (!treectl.spa || !a || !(a = a.getAttribute('href') + '') || !a.endsWith('/'))
+		return;
+
+	treectl.reqls(a, true, true);
+	return ev(e);
+};
+
+
 ebi('files').onclick = ebi('docul').onclick = function (e) {
 	var tgt = e.target.closest('a[id]');
 	if (tgt && tgt.getAttribute('id').indexOf('f-') === 0 && tgt.textContent.endsWith('/')) {
 		var el = treectl.find(tgt.textContent.slice(0, -1));
-		if (!el)
-			return;
-
-		el.click();
-		return ev(e);
+		if (el) {
+			el.click();
+			return ev(e);
+		}
+		if (treectl.spa) {
+			treectl.reqls(tgt.getAttribute('href'), true, true);
+			return ev(e);
+		}
+		return;
 	}
 
 	tgt = e.target.closest('a[hl]');
@@ -5001,7 +5018,7 @@ ebi('files').onclick = ebi('docul').onclick = function (e) {
 		showfile.show(noq_href(ebi(tgt.getAttribute('hl'))), tgt.getAttribute('lang'));
 		return ev(e);
 	}
-}
+};
 
 
 function reload_mp() {

commit 1d9b355743fdb6ab9cbf6f3646eeab3af50cc518

fix search ui after b265e59 broke it

copyparty/web/browser.js
@@ -1481,7 +1481,7 @@ function play(tid, is_ev, seek, call_depth) {
 			seek_au_sec(seek);
 		}
 
-		if (!seek) {
+		if (!seek && !ebi('unsearch')) {
 			var o = ebi(oid);
 			o.setAttribute('id', 'thx_js');
 			sethash(oid);
@@ -2966,9 +2966,6 @@ document.onkeydown = function (e) {
 		if (QS('.opview.act'))
 			return QS('#ops>a').click();
 
-		if (QS('#unsearch'))
-			return QS('#unsearch').click();
-
 		if (widget.is_open)
 			return widget.close();
 
@@ -2977,6 +2974,9 @@ document.onkeydown = function (e) {
 
 		if (!treectl.hidden)
 			return treectl.detree();
+
+		if (QS('#unsearch'))
+			return QS('#unsearch').click();
 
 		if (thegrid.en)
 			return ebi('griden').click();
@@ -3352,7 +3352,7 @@ document.onkeydown = function (e) {
 
 		treectl.hide();
 
-		var html = mk_files_header(tagord);
+		var html = mk_files_header(tagord), seen = {};
 		html.push('<tbody>');
 		html.push('<tr><td>-</td><td colspan="42"><a href="#" id="unsearch"><big style="font-weight:bold">[❌] close search results</big></a></td></tr>');
 		for (var a = 0; a < res.hits.length; a++) {
@@ -3361,13 +3361,18 @@ document.onkeydown = function (e) {
 				sz = esc(r.sz + ''),
 				rp = esc(uricom_dec(r.rp + '')[0]),
 				ext = rp.lastIndexOf('.') > 0 ? rp.split('.').pop().split('?')[0] : '%',
-				links = linksplit(r.rp + '');
+				id = 'f-' + ('00000000' + crc32(rp)).slice(-8);
+
+			while (seen[id])
+				id += 'a';
+			seen[id] = 1;
 
 			if (ext.length > 8)
 				ext = '%';
 
-			links = links.join('');
-			var nodes = ['<tr><td>-</td><td><div>' + links + '</div>', sz];
+			var links = linksplit(r.rp + '', id).join(''),
+				nodes = ['<tr><td>-</td><td><div>' + links + '</div>', sz];
+
 			for (var b = 0; b < tagord.length; b++) {
 				var k = tagord[b],
 					v = r.tags[k] || "";

copyparty/web/util.js
@@ -434,7 +434,7 @@ function makeSortable(table, cb) {
 }
 
 
-function linksplit(rp) {
+function linksplit(rp, id) {
     var ret = [],
         apath = '/',
         q = null;
@@ -464,8 +464,13 @@ function linksplit(rp) {
             vlink = vlink.slice(0, -1) + '<span>/</span>';
         }
 
-        if (!rp && q)
-            link += q;
+        if (!rp) {
+            if (q)
+                link += q;
+
+            if (id)
+                link += '" id="' + id;
+        }
 
         ret.push('<a href="' + apath + link + '">' + vlink + '</a>');
         apath += link;