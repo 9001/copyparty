e34634f5af034153801d2ca71f93835b9ba220cf
ed <s@ocv.me>
2022-04-15 19:42:40 +0200

    v1.2.5

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (1, 2, 4)
+VERSION = (1, 2, 5)
 CODENAME = "ftp btw"
-BUILD_DT = (2022, 4, 14)
+BUILD_DT = (2022, 4, 15)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
