feb1acd43a98d3bc421e5af51e973887d7143030
ed <s@ocv.me>
2022-06-27 22:57:05 +0200

    v1.3.3

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (1, 3, 2)
+VERSION = (1, 3, 3)
 CODENAME = "god dag"
-BUILD_DT = (2022, 6, 20)
+BUILD_DT = (2022, 6, 27)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
