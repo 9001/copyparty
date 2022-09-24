aaaa80e4b886160cab2d4d4bf73c843133b66f7d
ed <s@ocv.me>
2022-09-24 14:45:50 +0200

    v1.4.1

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (1, 4, 0)
+VERSION = (1, 4, 1)
 CODENAME = "mostly reliable"
-BUILD_DT = (2022, 9, 23)
+BUILD_DT = (2022, 9, 24)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
