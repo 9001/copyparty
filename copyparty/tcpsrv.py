a18f63895f392e6754524435a74b4afb0b7b010b
ed <s@ocv.me>
2023-12-21 00:48:51 +0100

    fix resource leak on macos

--- copyparty/tcpsrv.py
+++ copyparty/tcpsrv.py
@@ -241,6 +241,11 @@ class TcpSrv(object):
                 raise OSError(E_ADDR_IN_USE[0], "")
             self.srv.append(srv)
         except (OSError, socket.error) as ex:
+            try:
+                srv.close()
+            except:
+                pass
+
             if ex.errno in E_ADDR_IN_USE:
                 e = "\033[1;31mport {} is busy on interface {}\033[0m".format(port, ip)
             elif ex.errno in E_ADDR_NOT_AVAIL:
