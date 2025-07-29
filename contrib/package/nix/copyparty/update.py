48dfd73c9194e32b0cfbea9590c72d58fce8aefa
Toast <39011842+toast003@users.noreply.github.com>
2025-07-29 12:11:00 +0200

    nix: remove unused code from update.py

--- contrib/package/nix/copyparty/update.py
+++ contrib/package/nix/copyparty/update.py
@@ -2,23 +2,18 @@
 
 # Update the Nix package pin
 #
-# Usage: ./update.sh [PATH]
-# When the [PATH] is not set, it will fetch the latest release from the repo.
-# With [PATH] set, it will hash the given file and generate the URL,
-# base on the version contained within the file
+# Usage: ./update.sh
 
 import base64
 import json
 import hashlib
 import sys
-import re
 from pathlib import Path
 
 OUTPUT_FILE = Path("pin.json")
 TARGET_ASSET = lambda version: f"copyparty-{version}.tar.gz"
 HASH_TYPE = "sha256"
 LATEST_RELEASE_URL = "https://api.github.com/repos/9001/copyparty/releases/latest"
-DOWNLOAD_URL = lambda version: f"https://github.com/9001/copyparty/releases/download/v{version}/{TARGET_ASSET}"
 
 
 def get_formatted_hash(binary):
@@ -27,14 +22,6 @@ def get_formatted_hash(binary):
     asset_hash = hasher.digest()
     encoded_hash = base64.b64encode(asset_hash).decode("ascii")
     return f"{HASH_TYPE}-{encoded_hash}"
-
-
-def version_from_sfx(binary):
-    result = re.search(b'^VER = "(.*)"$', binary, re.MULTILINE)
-    if result:
-        return result.groups(1)[0].decode("ascii")
-
-    raise ValueError("version not found in provided file")
 
 
 def remote_release_pin():
@@ -51,22 +38,8 @@ def remote_release_pin():
     return result
 
 
-def local_release_pin(path):
-    asset = path.read_bytes()
-    version = version_from_sfx(asset)
-    download_url = DOWNLOAD_URL(version)
-    formatted_hash = get_formatted_hash(asset)
-
-    result = {"url": download_url, "version": version, "hash": formatted_hash}
-    return result
-
-
 def main():
-    if len(sys.argv) > 1:
-        asset_path = Path(sys.argv[1])
-        result = local_release_pin(asset_path)
-    else:
-        result = remote_release_pin()
+    result = remote_release_pin()
 
     print(result)
     json_result = json.dumps(result, indent=4)
