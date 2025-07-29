9858b714b448b72d2deb366062b612018c8ba8db
Toast <39011842+toast003@users.noreply.github.com>
2025-07-29 15:31:51 +0200

    nix: add partftpy package

--- flake.nix
+++ flake.nix
@@ -16,6 +16,19 @@
         copyparty = final.python3.pkgs.callPackage ./contrib/package/nix/copyparty {
           ffmpeg = final.ffmpeg-full;
         };
+        python3 = prev.python3.override {
+          packageOverrides = pyFinal: pyPrev: {
+            partftpy = pyFinal.tftpy.overrideAttrs {
+              pname = "partftpy";
+              version = "0.4.0";
+              src = final.fetchurl {
+                url = "https://github.com/9001/partftpy/releases/download/v0.4.0/partftpy-0.4.0.tar.gz";
+                hash = "sha256-5Q2zyuJ892PGZmb+YXg0ZPW/DK8RDL1uE0j5HPd4We0=";
+              };
+              pythonImportsCheck = [ "partftpy.TftpServer" ];
+            };
+          };
+        };
       };
     }
     // flake-utils.lib.eachDefaultSystem (
