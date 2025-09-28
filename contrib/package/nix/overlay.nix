a57661db1576f1e9927566adc5f7f7b40918f79e
Shelvacu <git@shelvacu.com>
2025-09-28 09:56:35 -0700

    copyparty-unstable build in nix overlay

--- contrib/package/nix/overlay.nix
+++ contrib/package/nix/overlay.nix
@@ -1,7 +1,27 @@
-final: prev: {
-  copyparty = final.python3.pkgs.callPackage ./copyparty {
-    ffmpeg = final.ffmpeg-full;
+final: prev:
+let
+  fullAttrs = {
+    withHashedPasswords = true;
+    withCertgen = true;
+    withThumbnails = true;
+    withFastThumbnails = true;
+    withMediaProcessing = true;
+    withBasicAudioMetadata = true;
+    withZeroMQ = true;
+    withFTP = true;
+    withFTPS = true;
+    withTFTP = true;
+    withSMB = true;
+    withMagic = true;
   };
+
+  call = attrs: final.python3.pkgs.callPackage ./copyparty ({ ffmpeg = final.ffmpeg-full; } // attrs);
+in
+{
+  copyparty = call { stable = true; };
+  copyparty-unstable = call { stable = false; };
+  copyparty-full = call (fullAttrs // { stable = true; });
+  copyparty-unstable-full = call (fullAttrs // { stable = false; });
 
   python3 = prev.python3.override {
     packageOverrides = pyFinal: pyPrev: {
