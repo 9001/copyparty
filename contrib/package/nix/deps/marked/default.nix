f8af462977ed419bcb992909d8439244f9cf080a
Toast <39011842+toast003@users.noreply.github.com>
2025-11-03 02:04:15 +0100

    Nix: inject DOMPurify into minified marked
    See https://github.com/9001/copyparty/blob/ac085b8149ff50e03d260128596dd130ed1c7cae/scripts/deps-docker/Dockerfile#L138C1-L138C81

--- contrib/package/nix/deps/marked/default.nix
+++ contrib/package/nix/deps/marked/default.nix
@@ -1,4 +1,4 @@
-{ buildNpmPackage, fetchFromGitHub }:
+{ buildNpmPackage, fetchFromGitHub, breakpointHook }:
 buildNpmPackage rec {
   pname = "marked";
   version = "4.3.0";
@@ -10,10 +10,23 @@ buildNpmPackage rec {
     hash = "sha256-xJotgIUoZSmjgLRIgJP9134PR1GYKjVXM5jp+JpM7bg=";
   };
 
+  dompurify = fetchFromGitHub {
+    owner = "cure53";
+    repo = "DOMPurify";
+    tag = "3.2.7";
+    hash = "sha256-lwBi/3uqWBa0UTPV1iKIIXDC6+Fc9uwIgs4+pRUjdPA=";
+  };
+
   npmDepsHash = "sha256-omsoER0I28ZHN78X0aku2jNJApPvpqYKgfPhRbEi+HM=";
 
   patches = [
     ./../../../../../scripts/deps-docker/marked.patch
     ./../../../../../scripts/deps-docker/marked-ln.patch
   ];
+
+  nativeBuildInputs = [ breakpointHook ];
+  postBuild = ''
+    # Inject DOMPurify to minified marked
+    cat ${dompurify}/dist/purify.min.js >> marked.min.js
+  '';
 }
