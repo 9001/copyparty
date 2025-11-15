8e1e61ed50a44d0bb32cbb77a66e35de73dbeb75
Toast <39011842+toast003@users.noreply.github.com>
2025-11-15 01:14:45 +0100

    Nix: apply patches to fusepy

--- contrib/package/nix/overlay.nix
+++ contrib/package/nix/overlay.nix
@@ -16,6 +16,17 @@ final: prev: {
   python3 = prev.python3.override {
     packageOverrides = pyFinal: pyPrev: {
       partftpy = pyFinal.callPackage ./partftpy { };
+      fusepy-copyparty = pyPrev.fusepy.overridePythonAttrs (old: {
+        pname = "${old.pname}-copyparty";
+        patchPhase = old.patchPhase + ''
+          python3 ${./../../../scripts/uncomment.py} fuse.py
+          sed -ri '/self.__critical_exception = e/d' fuse.py
+          awk '/^log =/{s=0} !s; /^from traceback im/{s=1;print"from functools import partial";print"basestring = str"}' < fuse.py > temp
+          awk '/LoggingMixIn:/{exit} --s<0;/self.use_ns = getattr/{s=7}' < temp > fuse.py
+          awk "/if _machine =/{s=0} /'(mips|ppc|ppc64)'/{s=1} !s" < fuse.py > temp
+          mv temp fuse.py
+        '';
+      });
     };
   };
 }
