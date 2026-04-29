ad4ece4f88c3dcb713040d87f642c527f7e14406
Til Schmitter <til.schmitter@gmail.com>
2026-04-29 22:39:53 +0200

    actions for selected item bar scrolls on small screens

--- copyparty/web/browser.js
+++ copyparty/web/browser.js
@@ -896,41 +896,46 @@ var svg_pause = svg_box + '<path d="M10 9V15M14 9V15M12 21C16.9706 21 21 16.9706
 var svg_vol = svg_box + '<path d="M16.0004 9.00009C16.6281 9.83575 17 10.8745 17 12.0001C17 13.1257 16.6281 14.1644 16.0004 15.0001M18 5.29177C19.8412 6.93973 21 9.33459 21 12.0001C21 14.6656 19.8412 17.0604 18 18.7084M4.6 9.00009H5.5012C6.05213 9.00009 6.32759 9.00009 6.58285 8.93141C6.80903 8.87056 7.02275 8.77046 7.21429 8.63566C7.43047 8.48353 7.60681 8.27191 7.95951 7.84868L10.5854 4.69758C11.0211 4.17476 11.2389 3.91335 11.4292 3.88614C11.594 3.86258 11.7597 3.92258 11.8712 4.04617C12 4.18889 12 4.52917 12 5.20973V18.7904C12 19.471 12 19.8113 11.8712 19.954C11.7597 20.0776 11.594 20.1376 11.4292 20.114C11.239 20.0868 11.0211 19.8254 10.5854 19.3026L7.95951 16.1515C7.60681 15.7283 7.43047 15.5166 7.21429 15.3645C7.02275 15.2297 6.80903 15.1296 6.58285 15.0688C6.32759 15.0001 6.05213 15.0001 5.5012 15.0001H4.6C4.03995 15.0001 3.75992 15.0001 3.54601 14.8911C3.35785 14.7952 3.20487 14.6422 3.10899 14.4541C3 14.2402 3 13.9601 3 13.4001V10.6001C3 10.04 3 9.76001 3.10899 9.54609C3.20487 9.35793 3.35785 9.20495 3.54601 9.10908C3.75992 9.00009 4.03995 9.00009 4.6 9.00009Z" ' + svg_options + '/></svg>'
 
 ebi('widget').innerHTML = (
-	'<div id="wtoggle">' +
+	'<div id="wstack">' +
 
-	'<div id="up_quick">' +
-	'	<div id="up_quick_more">' +
-	'		<a id="uq_nd">' + L.rc_nfo + ' <span><p class="overlay_plus">+</p>📂</span></a>\n' +
-	'		<a id="uq_nf">' + L.rc_nfi + ' <span><p class="overlay_plus">+</p>📝</span></a>\n' +
-	'		<a id="uq_up">' + L.cl_uopts + ' 🚀</a>\n' +
-	'	</div>' +
-	'	<a id="up_quick_btn" class="tgl btn on"><div class="rotatable">+</div></a>' +
+		'<div id="up_quick">' +
+		'	<div id="up_quick_more">' +
+		'		<a id="uq_nd">' + L.rc_nfo + ' <span><p class="overlay_plus">+</p>📂</span></a>\n' +
+		'		<a id="uq_nf">' + L.rc_nfi + ' <span><p class="overlay_plus">+</p>📝</span></a>\n' +
+		'		<a id="uq_up">' + L.cl_uopts + ' 🚀</a>\n' +
+		'	</div>' +
+		'	<a id="up_quick_btn" class="tgl btn on"><div class="rotatable">+</div></a>' +
+		'</div>' +
+
+		'<div id="wtoggle">' +
+		'<span id="wfs"></span>' +
+			'<span id="wfm"><a' +
+				' href="#" id="fshr" tt="' + L.wt_shr + '">📨<span>share</span></a><a' +
+				' href="#" id="fren" tt="' + L.wt_ren + '">✎<span>name</span></a><a' +
+				' href="#" id="fdel" tt="' + L.wt_del + '">🗑️<span>del.</span></a><a' +
+				' href="#" id="fcut" tt="' + L.wt_cut + '">✂<span>cut</span></a><a' +
+				' href="#" id="fcpy" tt="' + L.wt_cpy + '">⧉<span>copy</span></a><a' +
+				' href="#" id="fpst" tt="' + L.wt_pst + '">📋<span>paste</span></a>' +
+			'</span><span id="wzip1"><a' +
+				' href="#" id="zip1" tt="' + L.wt_zip1 + '">⬇️📦<span>zip</span></a>' +
+			'</span><span id="wzip"><a' +
+				' href="#" id="selall" tt="' + L.wt_selall + '">sel.<br />all</a><a' +
+				' href="#" id="selinv" tt="' + L.wt_selinv + '">sel.<br />inv.</a><a' +
+				' href="#" id="selzip" class="l1" tt="' + L.wt_selzip + '">⬇️zip</a><a' +
+				' href="#" id="seldl" class="l1" tt="' + L.wt_seldl + '">⬇️files</a>' +
+			'</span><span id="wnp"><a' +
+				' href="#" id="npirc" tt="' + L.wt_npirc + '">📋<span>irc</span></a><a' +
+				' href="#" id="nptxt" tt="' + L.wt_nptxt + '">📋<span>txt</span></a>' +
+			'</span><span id="wm3u"><a' +
+				' href="#" id="m3ua" tt="' + L.wt_m3ua + '">📻<span>add</span></a><a' +
+				' href="#" id="m3uc" tt="' + L.wt_m3uc + '">📻<span>copy</span></a>' +
+			'</span>' +
+		'</div>' +
+
+		'<a href="#" id="wtico">♫</a>' +
+
 	'</div>' +
 
-	'<span id="wfs"></span>' +
-	'<span id="wfm"><a' +
-	' href="#" id="fshr" tt="' + L.wt_shr + '">📨<span>share</span></a><a' +
-	' href="#" id="fren" tt="' + L.wt_ren + '">✎<span>name</span></a><a' +
-	' href="#" id="fdel" tt="' + L.wt_del + '">🗑️<span>del.</span></a><a' +
-	' href="#" id="fcut" tt="' + L.wt_cut + '">✂<span>cut</span></a><a' +
-	' href="#" id="fcpy" tt="' + L.wt_cpy + '">⧉<span>copy</span></a><a' +
-	' href="#" id="fpst" tt="' + L.wt_pst + '">📋<span>paste</span></a>' +
-	'</span><span id="wzip1"><a' +
-	' href="#" id="zip1" tt="' + L.wt_zip1 + '">⬇️📦<span>zip</span></a>' +
-	'</span><span id="wzip"><a' +
-	' href="#" id="selall" tt="' + L.wt_selall + '">sel.<br />all</a><a' +
-	' href="#" id="selinv" tt="' + L.wt_selinv + '">sel.<br />inv.</a><a' +
-	' href="#" id="selzip" class="l1" tt="' + L.wt_selzip + '">⬇️zip</a><a' +
-	' href="#" id="seldl" class="l1" tt="' + L.wt_seldl + '">⬇️files</a>' +
-	'</span><span id="wnp"><a' +
-	' href="#" id="npirc" tt="' + L.wt_npirc + '">📋<span>irc</span></a><a' +
-	' href="#" id="nptxt" tt="' + L.wt_nptxt + '">📋<span>txt</span></a>' +
-	'</span><span id="wm3u"><a' +
-	' href="#" id="m3ua" tt="' + L.wt_m3ua + '">📻<span>add</span></a><a' +
-	' href="#" id="m3uc" tt="' + L.wt_m3uc + '">📻<span>copy</span></a>' +
-	'</span><a' +
-	'	href="#" id="wtico">♫</a>' +
-	'</div>' +
 	'<div id="widgeti">' +
 	'	<div id="pbarthinpos"></div>' +
 	'	<div id="pctl">' +
@@ -964,6 +969,10 @@ ebi('widget').innerHTML = (
 	'	<span id="np_dur"></span>' +
 	'</div>'
 );
+
+ebi('wtoggle').addEventListener('wheel', function (e) {
+	scroll_v_to_h(e, this);
+});
 
 ebi('up_quick').onclick = function(){
 	var btn = ebi('up_quick_btn');
