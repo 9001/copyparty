78de02123eb128847e1519004bf5eac574e94369
Til Schmitter <til.schmitter@gmail.com>
2026-05-25 00:55:25 +0200

    undo last commit, set bbox image fetch prio to high

--- copyparty/web/baguettebox.js
+++ copyparty/web/baguettebox.js
@@ -691,6 +691,7 @@ window.baguetteBox = (function () {
         for (var i = 0, fullImage; i < gallery.length; i++) {
             fullImage = mknod('div', 'baguette-img-' + i);
             fullImage.className = 'full-image';
+            fullImage.fetchPriority = 'high';
             imagesElements.push(fullImage);
 
             imagesFiguresIds.push('bbox-figure-' + i);
