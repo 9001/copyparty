27682ec847f122050bdf40ee56559c31e86b089d
Danila Kamaev <github@kamaeff.com>
2026-06-01 18:08:19 +0400

    feat: support ps4 fPKG thumbnails

--- copyparty/mtag.py
+++ copyparty/mtag.py
@@ -2,10 +2,12 @@
 from __future__ import division, print_function, unicode_literals
 
 import argparse
+from io import BytesIO
 import json
 import os
 import re
 import shutil
+import struct
 import subprocess as sp
 import sys
 import tempfile
@@ -203,6 +205,8 @@ def au_unpk(
         elif pk == "epub":
             fi = get_cover_from_epub(log, abspath)
             assert fi  # !rm
+        elif pk == "pkg":
+            fi = get_cover_from_pkg(abspath)
 
         else:
             raise Exception("unknown compression %s" % (pk,))
@@ -499,6 +503,46 @@ def _get_cover_from_epub2(
             return cover_path
 
     return None
+
+
+def get_cover_from_pkg(abspath: str) -> IO[bytes]:
+    FPKG_MAGIC = 0x7F434E54
+    ENTRY_ID_ICON0_PNG = 0x1200
+    ENTRY_ID_PIC0_PNG = 0x1220
+    # limits for sanity checks
+    ENTRY_COUNT_LIMIT = 255
+    FILE_SIZE_LIMIT = 10_000_000
+
+    with open(abspath, 'rb') as f:
+        magic, entry_count, entry_table_position = _read_struct(f, '>I12xI4xI')
+        if magic != FPKG_MAGIC:
+            raise Exception("pkg file: not a playstation 4 fPKG: %s" % abspath)
+        if entry_count > ENTRY_COUNT_LIMIT:
+            raise Exception("pkg file: suspicious; too many entries: %s" % abspath)
+
+        cover_offset = cover_length = None
+        f.seek(entry_table_position)
+        for _ in range(entry_count):
+            entry_id, offset, length = _read_struct(f, '>I12xII8x')
+            if entry_id not in {ENTRY_ID_ICON0_PNG, ENTRY_ID_PIC0_PNG}:
+                continue
+            cover_offset, cover_length = offset, length
+            # prefer icon0.png
+            if entry_id == ENTRY_ID_ICON0_PNG:
+                break
+
+        if cover_length is None:
+            raise Exception("pkg file: no cover image found: %s" % abspath)
+        if cover_length > FILE_SIZE_LIMIT:
+            raise Exception("pkg file: suspicious; cover image too large: %s" % abspath)
+
+        f.seek(cover_offset)
+        return BytesIO(f.read(cover_length))
+
+
+def _read_struct(file: IO[bytes], fmt: str) -> tuple[int, ...]:
+    size = struct.calcsize(fmt)
+    return struct.unpack(fmt, file.read(size))
 
 
 class MTag(object):
--- copyparty/up2k.py
+++ copyparty/up2k.py
@@ -104,7 +104,7 @@ ICV_EXTS = set(zsg.split(","))
 zsg = "3gp,asf,av1,avc,avi,flv,m4v,mjpeg,mjpg,mkv,mov,mp4,mpeg,mpeg2,mpegts,mpg,mpg2,mts,nut,ogm,ogv,rm,vob,webm,wmv"
 VCV_EXTS = set(zsg.split(","))
 
-zsg = "aif,aiff,alac,ape,flac,m4a,m4b,m4r,mka,mp3,oga,ogg,opus,tak,tta,wav,wma,wv,cbz,epub"
+zsg = "aif,aiff,alac,ape,flac,m4a,m4b,m4r,mka,mp3,oga,ogg,opus,tak,tta,wav,wma,wv,cbz,epub,pkg"
 ACV_EXTS = set(zsg.split(","))
 
 zsg = "nohash noidx xdev xvol"
