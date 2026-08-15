efcf96e3e32374e8416586afed9177cd7f9263e7
Danila Kamaev <github@kamaeff.com>
2026-08-15 17:06:45 +0400

    wopi: collaborate (#1591)
    persistent file IDs for real-time collaboration

--- copyparty/httpcli.py
+++ copyparty/httpcli.py
@@ -1649,7 +1649,8 @@ class HttpCli(object):
                 raise Pebkac(500, "too many wopi sessions")
             if not found:
                 atoken = ub64enc(os.urandom(18)).decode("ascii")  #  18 = 144b = 24c
-                file_id = ub64enc(os.urandom(15)).decode("ascii")  # 15 = 120b = 20c
+                zb = (vpath + self.args.wopi_salt).encode("utf-8", "replace")
+                file_id = ub64enc(hashlib.sha512(zb).digest()[:15]).decode("ascii")
                 wopi_files[atoken] = session = {
                     "vp": vpath,
                     "uname": self.uname,
