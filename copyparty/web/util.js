ab791921ea92371408ac803a4762c8638c046043
ed <s@ocv.me>
2026-09-04 17:54:20 +0000

    fix csp-nonce on chrome<62

--- copyparty/web/util.js
+++ copyparty/web/util.js
@@ -46,6 +46,9 @@ var wah = '',
 if (!window.WebAssembly || !WebAssembly.Memory)
     window.WebAssembly = false;
 
+if (!window.AbortController)
+    window.AbortController = false;
+
 if (!window.Notification || !Notification.permission)
     window.Notification = false;
 
@@ -450,8 +453,11 @@ function import_js(url, cb, ecb) {
     var head = document.head || document.getElementsByTagName('head')[0];
     var script = mknod('script');
     script.type = 'text/javascript';
-    if (window.JS_NONCE)
+    if (window.JS_NONCE) {
         script.nonce = JS_NONCE;
+        if (!AbortController)
+            script.setAttribute('nonce', JS_NONCE);
+    }
     script.src = url + '?_=' + (window.TS || 'a');
     script.onload = cb;
     script.onerror = ecb || function () {
