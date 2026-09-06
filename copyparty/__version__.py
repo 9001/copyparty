de21c210c60a5198a3b943586e423820dfeba162
ed <s@ocv.me>
2026-09-06 21:12:32 +0000

    v1.20.22

--- copyparty/__version__.py
+++ copyparty/__version__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-VERSION = (1, 20, 21)
+VERSION = (1, 20, 22)
 CODENAME = "sftp is fine too"
-BUILD_DT = (2026, 8, 17)
+BUILD_DT = (2026, 9, 6)
 
 S_VERSION = ".".join(map(str, VERSION))
 S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
