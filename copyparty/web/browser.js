1217b147be9befdb1afac31b02504786ffeec2c6
ed <s@ocv.me>
2026-09-18 19:45:31 +0000

    Update browser.js
    Signed-off-by: ed <s@ocv.me>

--- copyparty/web/browser.js
+++ copyparty/web/browser.js
@@ -5077,19 +5077,22 @@ var showfile = (function () {
 	r.nmap = {
 		'dockerfile': 'docker'
 	};
-	r.smap = {
+	r.pmap = {
 		'sh': 'bash',
-		'bash': 'bash',
-		'python': 'python',
-		'python3': 'python',
-		'perl': 'perl',
-		'ruby': 'ruby',
+		'ash': 'bash',
+		'dash': 'bash',
+		'luajit': 'lua',
+		'node': 'js',
 	};
 	var x = txt_ext + ' ans c cfg conf cpp cs css diff glsl go html ini java js json jsx kt kts latex less lisp lua makefile md nasm nim nix py r rss rb ruby sass scss sql svg swift tex toml ts vhdl xml yaml zig';
 	x = x.split(/ +/g);
 	for (var a = 0; a < x.length; a++)
 		if (!r.map["." + x[a]])
 			r.map["." + x[a]] = x[a];
+	x = 'awk bash lua make perl python ruby swift';
+	x = x.split(/ +/g);
+	for (var a = 0; a < x.length; a++)
+		r.pmap[x[a]] = x[a];
 
 	r.sname = function (srch) {
 		return srch.split(/[?&]doc=/)[1].split('&')[0];
@@ -5132,17 +5135,14 @@ var showfile = (function () {
 	r.getlang = function (fn, txt) {
 		fn = fn.toLowerCase();
 		var ext = fn.slice(fn.lastIndexOf('.'));
-		var shebang = txt && txt.startsWith("#!")
-			? txt
-					.split('\n')[0]
-					.replace('#!/usr/bin/env', '')
-					.replace('#!/usr/bin/', '')
-					.replace('#!/bin/', '')
-					.trim()
-					.split(' ')[0]
-			: null;
-		return r.map[ext] || r.nmap[fn] || r.smap[shebang];
-	}
+		var prog = txt && txt.startsWith('#!') && txt.split('\n')[0].replace(
+			'#!/usr/bin/env', '').replace(
+			'#!/usr/bin/', '').replace(
+			'#!/bin/', '').trim().split(' ')[0];
+		if (prog)
+			prog = r.pmap[prog] || r.pmap[prog.replace(/[0-9].*/, '')];
+		return r.map[ext] || r.nmap[fn] || prog;
+	};
 
 	r.addlinks = function () {
 		r.files = [];
