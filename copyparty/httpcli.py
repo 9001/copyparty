f445bfa862f086839fe6cfb2cdc2409e788d936a
willie6666 <89621369+willie6666@users.noreply.github.com>
2026-09-20 09:12:56 -0400

    shares: delete sf entries too (#1647)
    Signed-off-by: willie6666 <89621369+willie6666@users.noreply.github.com>

--- copyparty/httpcli.py
+++ copyparty/httpcli.py
@@ -6664,6 +6664,7 @@ class HttpCli(object):
         act = self.uparam["eshare"]
         if act == "rm":
             cur.execute("delete from sh where k = ?", (skey,))
+            cur.execute("delete from sf where k = ?", (skey,))
             if skey in self.asrv.vfs.nodes[self.args.shr.strip("/")].nodes:
                 reload = True
         else:
